20da480c0fe8838e721a2a1a202b0c0102d4a1d0
Shreyash Thengne <shreyashthengne@gmail.com>
2024-06-26 17:10:29 +0530

    for render

--- app.py
+++ app.py
@@ -308,4 +308,6 @@ def recomm(emotion):
 
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    # app.run(debug = True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
