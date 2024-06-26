2624e76b6d8dd2f14243f30162920d0868ec4104
Shreyash Thengne <shreyashthengne@gmail.com>
2024-06-26 17:49:57 +0530

    vercel app deployment

--- app.py
+++ app.py
@@ -307,7 +307,7 @@ def recomm(emotion):
     return render_template('recomm.html', zipped = zip(recommendations['name'], recommendations['id']), emotion=emotion)
 
 
-if __name__ == '__main__':
-    # app.run(debug = True)
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+# if __name__ == '__main__':
+#     app.run(debug = True)
+    # port = int(os.environ.get("PORT", 5000))
+    # app.run(host="0.0.0.0", port=port)
